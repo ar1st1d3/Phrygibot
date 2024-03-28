d8c5bea143cf41d065cc87e44c8470352677f981
president pug <93650137+ar1st1d3@users.noreply.github.com>
2024-03-28 16:47:04 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,26 @@
 # Base de donnée des sports olympiques
 
 '''
+    Ce fichier python sert de base de donnée pour le programme phrygibot
+    Copyright (C) 2024  Spreng
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
+
+'''
 Exemple de formation du dictionnaire de chaque sports :
 'sport' : {
         'name' : 'nom complet du sport',
